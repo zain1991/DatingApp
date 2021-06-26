b4d4547cea4364f35c040072ad166b8496de1c8e
zain1991 <zainislam14@gmail.com>
2021-06-27 00:56:30 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
                 optionsAction.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,7 +52,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'The Dating App';
+  users:any;
+  constructor(private http: HttpClient){}
+  
+  ngOnInit(): void {
+    this.getusers();
+  }
+  getusers(){
+    this.http.get("https://localhost:5001/api/users").subscribe(response=>{
+    this.users = response;
+    },error=>{
+      console.log(error);
+    });
+  }
+}
